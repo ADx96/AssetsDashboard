ef9d1623f3ce69b0212adfff9e86ac540f38cf8e
Abduljawad AbuAlrous <abduljawad.abualrous@kw.zain.com>
2023-11-04 13:51:55 +0300

    new list item

--- src/Pages/Assets.jsx
+++ src/Pages/Assets.jsx
@@ -1,43 +1,43 @@
-import DataTable from "../components/DataTable";
-import AddModal from "../components/AddModal";
-import AssetsForm from "../components/Forms/AssetsForm";
+import DataTable from '../components/DataTable';
+import AddModal from '../components/AddModal';
+import AssetsForm from '../components/Forms/AssetsForm';
 import {
   useDeleteAssetMutation,
   useGetAssetsQuery,
   useUpdateAssetMutation,
-} from "../Redux/Api/AssetsApi";
-import { ContextProvider } from "../Hooks/ContextProvider";
-import { Space, Button, message, Popconfirm, Form, Input, Select } from "antd";
-import { EditableCell } from "../components/Forms/Editable";
-import { useState } from "react";
-import qs from "qs";
+} from '../Redux/Api/AssetsApi';
+import { ContextProvider } from '../Hooks/ContextProvider';
+import { Space, Button, message, Popconfirm, Form, Input, Select } from 'antd';
+import { EditableCell } from '../components/Forms/Editable';
+import { useState } from 'react';
+import qs from 'qs';
 
 const Assets = () => {
   const [form] = Form.useForm();
   const [deleteAsset] = useDeleteAssetMutation();
   const [updateAsset] = useUpdateAssetMutation();
   const [search, setSearch] = useState({
-    value: "",
-    selected: "",
+    value: '',
+    selected: '',
   });
 
   const { Search } = Input;
   const { success } = message;
   const [currentPage, setCurrentPage] = useState();
-  const [editingKey, setEditingKey] = useState("");
+  const [editingKey, setEditingKey] = useState('');
   const isEditing = (record) => record.id === editingKey;
 
   const filter = {
     filters: {
       Serial: {
-        $endsWith: search.selected === "serial" ? search.value : "",
+        $endsWith: search.selected === 'serial' ? search.value : '',
       },
       employee: {
         Name: {
-          $contains: search.selected === "name" ? search.value : "",
+          $contains: search.selected === 'name' ? search.value : '',
         },
         EmployeeId: {
-          $eq: search.selected === "id" ? search.value : "",
+          $eq: search.selected === 'id' ? search.value : '',
         },
       },
       isDropped: {
@@ -48,9 +48,9 @@ const Assets = () => {
   const checkedFilter = (obj) => {
     Object.keys(obj).forEach((key) => {
       const value = obj[key];
-      if (typeof value === "object") {
+      if (typeof value === 'object') {
         checkedFilter(value); // recursively check nested object properties
-      } else if (value === "") {
+      } else if (value === '') {
         delete obj[key]; // delete the key if the value matches the given string
       }
     });
@@ -59,7 +59,7 @@ const Assets = () => {
   const newFilter = checkedFilter(filter);
   const query = qs.stringify(
     {
-      populate: "employee",
+      populate: 'employee',
       ...newFilter,
       pagination: {
         page: currentPage,
@@ -82,17 +82,17 @@ const Assets = () => {
 
   const handleDelete = async (id) => {
     deleteAsset(id);
-    success("تم الحذف بنجاح");
+    success('تم الحذف بنجاح');
   };
 
   const save = async (key) => {
     const Submit = await form.validateFields();
     const id = editingKey;
     const NewData = { Submit, id };
-    success("تم التعديل بنجاح");
+    success('تم التعديل بنجاح');
     updateAsset(NewData);
-    success("تم التعديل بنجاح");
-    setEditingKey("");
+    success('تم التعديل بنجاح');
+    setEditingKey('');
   };
 
   const edit = (record) => {
@@ -108,98 +108,98 @@ const Assets = () => {
     setEditingKey(record.id);
   };
   const cancel = () => {
-    setEditingKey("");
+    setEditingKey('');
   };
 
   const columns = [
     {
-      title: "Employee Name",
-      dataIndex: "Name",
-      key: "Name",
-      align: "center",
+      title: 'Employee Name',
+      dataIndex: 'Name',
+      key: 'Name',
+      align: 'center',
     },
     {
-      title: "Employee I.D",
-      dataIndex: "EmployeeId",
-      key: "EmployeeId",
-      align: "center",
+      title: 'Employee I.D',
+      dataIndex: 'EmployeeId',
+      key: 'EmployeeId',
+      align: 'center',
     },
     {
-      title: "SERIAL NUMBER",
-      dataIndex: "Serial",
-      key: "Serial",
+      title: 'SERIAL NUMBER',
+      dataIndex: 'Serial',
+      key: 'Serial',
       editable: true,
-      align: "center",
+      align: 'center',
     },
     {
-      title: "ITEM",
-      dataIndex: "ItemName",
-      key: "ItemName",
+      title: 'ITEM',
+      dataIndex: 'ItemName',
+      key: 'ItemName',
       editable: true,
-      align: "center",
+      align: 'center',
     },
     {
-      title: "SPECIFICATION",
-      dataIndex: "Specs",
-      key: "Specs",
+      title: 'SPECIFICATION',
+      dataIndex: 'Specs',
+      key: 'Specs',
       editable: true,
-      align: "center",
+      align: 'center',
     },
     {
-      title: "OS",
-      dataIndex: "os",
-      key: "os",
-      align: "center",
-      editable: true,
-    },
-    {
-      title: "BUILDING",
-      dataIndex: "Building",
-      key: "Building",
-      editable: true,
-      align: "center",
-    },
-    {
-      title: "FlOOR",
-      dataIndex: "Floor",
-      key: "Floor",
-      align: "center",
+      title: 'OS',
+      dataIndex: 'os',
+      key: 'os',
+      align: 'center',
       editable: true,
     },
     {
-      title: "OFFICE",
-      dataIndex: "Office",
-      key: "Office",
+      title: 'BUILDING',
+      dataIndex: 'Building',
+      key: 'Building',
       editable: true,
-      align: "center",
+      align: 'center',
     },
     {
-      title: "Status",
-      dataIndex: "status",
-      key: "Office",
-      align: "center",
+      title: 'FlOOR',
+      dataIndex: 'Floor',
+      key: 'Floor',
+      align: 'center',
+      editable: true,
     },
     {
-      title: "Add Date",
-      dataIndex: "createdAt",
-      key: "createdAt",
-      align: "center",
+      title: 'OFFICE',
+      dataIndex: 'Office',
+      key: 'Office',
+      editable: true,
+      align: 'center',
     },
     {
-      title: "Action",
-      align: "center",
-      dataIndex: "operation",
-      key: "operation",
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'Office',
+      align: 'center',
+    },
+    {
+      title: 'Add Date',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      align: 'center',
+    },
+    {
+      title: 'Action',
+      align: 'center',
+      dataIndex: 'operation',
+      key: 'operation',
       render: (_, record) => {
         const editable = isEditing(record);
 
         return (
           <div>
             {editable ? (
-              <div style={{ display: "flex", justifyContent: "center" }}>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Space>
                   <Popconfirm
-                    title="Sure to save edit?"
+                    title='Sure to save edit?'
                     onConfirm={() => save(record.key)}
                   >
                     <Button
@@ -215,16 +215,16 @@ const Assets = () => {
                 </Space>
               </div>
             ) : (
-              <div style={{ display: "flex", justifyContent: "center" }}>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Space>
                   <Button
-                    disabled={editingKey !== ""}
+                    disabled={editingKey !== ''}
                     onClick={() => edit(record)}
                   >
                     Edit
                   </Button>
                   <Popconfirm
-                    title="Sure to delete?"
+                    title='Sure to delete?'
                     onConfirm={() => handleDelete(record.id)}
                   >
                     <Button>Delete</Button>
@@ -246,7 +246,7 @@ const Assets = () => {
       ...col,
       onCell: (record) => ({
         record,
-        inputType: col.dataIndex === "age" ? "number" : "text",
+        inputType: col.dataIndex === 'age' ? 'number' : 'text',
         dataIndex: col.dataIndex,
         title: col.title,
         editing: isEditing(record),
@@ -263,36 +263,36 @@ const Assets = () => {
 
   return (
     <ContextProvider>
-      <div style={{ display: "flex", justifyContent: "end" }}>
+      <div style={{ display: 'flex', justifyContent: 'end' }}>
         <Space>
-          <AddModal title={"اضافة بيانات العهدة"} children={<AssetsForm />} />
-          <div style={{ paddingBottom: "30px" }}>
+          <AddModal title={'اضافة بيانات العهدة'} children={<AssetsForm />} />
+          <div style={{ paddingBottom: '30px' }}>
             <Button
               onClick={() => refetch()}
-              style={{ borderRadius: "5px", textAlign: "left" }}
-              type="primary"
-              size={"large"}
+              style={{ borderRadius: '5px', textAlign: 'left' }}
+              type='primary'
+              size={'large'}
             >
               Refresh
             </Button>
           </div>
         </Space>
       </div>
-      <div style={{ overflow: "auto" }}>
+      <div style={{ overflow: 'auto' }}>
         <Search
-          style={{ width: "40%" }}
-          placeholder="Search By Serial and EmployeeId"
+          style={{ width: '40%' }}
+          placeholder='Search By Serial and EmployeeId'
           onSearch={(value) => {
             setSearch({ ...search, value: value });
           }}
         />
         <Select
           style={{ width: 150 }}
-          placeholder="Select for search"
+          placeholder='Select for search'
           options={[
-            { value: "id", label: "Employee Id" },
-            { value: "name", label: "Employee Name" },
-            { value: "serial", label: "Serial" },
+            { value: 'id', label: 'Employee Id' },
+            { value: 'name', label: 'Employee Name' },
+            { value: 'serial', label: 'Serial' },
           ]}
           onSelect={(value) => {
             setSearch({ ...search, selected: value });
--- src/components/Forms/RequestAssetDataForm.jsx
+++ src/components/Forms/RequestAssetDataForm.jsx
@@ -1,22 +1,22 @@
-import React, { createRef, useState, useRef } from "react";
-import { Form, Button, Input, Select } from "antd";
-import ReportsTable from "../ReportsTable";
-import ReportsExport from "../dashboard/ReportsExport";
-import { useGetAssetsQuery } from "../../Redux/Api/AssetsApi";
-import qs from "qs";
+import React, { createRef, useState, useRef } from 'react';
+import { Form, Button, Input, Select } from 'antd';
+import ReportsTable from '../ReportsTable';
+import ReportsExport from '../dashboard/ReportsExport';
+import { useGetAssetsQuery } from '../../Redux/Api/AssetsApi';
+import qs from 'qs';
 
 const RequestAssetDataForm = () => {
   const pdfRef = useRef(null);
   const formRef = createRef();
   const { Option } = Select;
-  const [value, setValues] = useState("");
+  const [value, setValues] = useState('');
 
   const checkLength =
     value.text?.length > 5
       ? {
-          $contains: value.selected === "Serial" ? value.text : "",
+          $contains: value.selected === 'Serial' ? value.text : '',
         }
-      : { $eq: value.selected === "Serial" ? value.text : "" };
+      : { $eq: value.selected === 'Serial' ? value.text : '' };
 
   const filter = {
     filters: {
@@ -25,29 +25,29 @@ const RequestAssetDataForm = () => {
       },
       ItemName: {
         $contains:
-          value.selected === "ItemName" ? value.text : "" || value.ItemName,
+          value.selected === 'ItemName' ? value.text : '' || value.ItemName,
       },
       Floor: {
-        $eq: value.selected === "Floor" ? value.text : "",
+        $eq: value.selected === 'Floor' ? value.text : '',
       },
       Building: {
-        $eq: value.selected === "Building" ? value.text : "",
+        $eq: value.selected === 'Building' ? value.text : '',
       },
       Office: {
-        $contains: value.selected === "Office" ? value.text : "",
+        $contains: value.selected === 'Office' ? value.text : '',
       },
       employee: {
         Name: {
-          $contains: value.selected === "Name" ? value.text : "",
+          $contains: value.selected === 'Name' ? value.text : '',
         },
         EmployeeId: {
-          $contains: value.selected === "EmployeeId" ? value.text : "",
+          $contains: value.selected === 'EmployeeId' ? value.text : '',
         },
         JobTitle: {
-          $contains: value.selected === "JobTitle" ? value.text : "",
+          $contains: value.selected === 'JobTitle' ? value.text : '',
         },
         WorkPlace: {
-          $contains: value.selected === "WorkPlace" ? value.text : "",
+          $contains: value.selected === 'WorkPlace' ? value.text : '',
         },
       },
     },
@@ -56,9 +56,9 @@ const RequestAssetDataForm = () => {
   const checkedFilter = (obj) => {
     Object.keys(obj).forEach((key) => {
       const value = obj[key];
-      if (typeof value === "object") {
+      if (typeof value === 'object') {
         checkedFilter(value); // recursively check nested object properties
-      } else if (value === "") {
+      } else if (value === '') {
         delete obj[key]; // delete the key if the value matches the given string
       }
     });
@@ -67,7 +67,7 @@ const RequestAssetDataForm = () => {
   const newFilter = checkedFilter(filter);
   const query = qs.stringify(
     {
-      populate: "employee",
+      populate: 'employee',
       ...newFilter,
       pagination: {
         pageSize: 1200,
@@ -80,15 +80,38 @@ const RequestAssetDataForm = () => {
 
   const { data, isLoading, refetch } = useGetAssetsQuery(query);
 
+  if (isLoading) {
+    return <></>;
+  }
+
+  const combinedArray = data.data.map((data) => {
+    const newData = data.attributes.employee.data?.attributes.JobTitle;
+    return newData;
+  });
+
+  const itemName = data.data.map((data) => {
+    const newData = data.attributes.ItemName;
+    return newData;
+  });
+
+  const newItemName = [...new Set(itemName)]
+    .filter((value) => value !== undefined)
+    .map((item) => item.replace(/\t/g, '').trim());
+
+  const uniqueArray = [...new Set(combinedArray)]
+    .filter((value) => value !== undefined)
+    .map((item) => item.replace(/\t/g, '').trim());
+
   const onFinish = async (values) => {
     setValues(values);
     formRef.current?.resetFields();
     await refetch();
   };
+
   return (
     <>
       {value.text ? (
-        <div style={{ overflow: "auto" }}>
+        <div style={{ overflow: 'auto' }}>
           <ReportsTable
             data={data}
             value={value}
@@ -101,10 +124,10 @@ const RequestAssetDataForm = () => {
       ) : (
         <>
           <Form
-            name="basic"
+            name='basic'
             ref={formRef}
-            layout="vertical"
-            style={{ textAlign: "center" }}
+            layout='vertical'
+            style={{ textAlign: 'center' }}
             initialValues={{ remember: false }}
             onFinish={onFinish}
             labelCol={{
@@ -120,53 +143,108 @@ const RequestAssetDataForm = () => {
                 offset: 8,
               },
             }}
-            autoComplete="off"
+            autoComplete='off'
           >
-            <Form.Item label="بحث عن طريق" name="selected">
+            <Form.Item label='بحث عن طريق' name='selected'>
               <Select onChange={(val) => setValues(val)}>
-                <Option value={"Name"}>Employee Name</Option>
-                <Option value={"EmployeeId"}>Employee ID</Option>
-                <Option value={"Serial"}>Serial Number</Option>
-                <Option value={"ItemName"}>Item Name</Option>
-                <Option value={"Floor"}>Floor</Option>
-                <Option value={"Building"}>Building</Option>
-                <Option value={"Office"}>Office</Option>
-                <Option value={"JobTitle"}>Job Title</Option>
-                <Option value={"WorkPlace"}>Work Place</Option>
+                <Option value={'Name'}>Employee Name</Option>
+                <Option value={'EmployeeId'}>Employee ID</Option>
+                <Option value={'Serial'}>Serial Number</Option>
+                <Option value={'ItemName'}>Item Name</Option>
+                <Option value={'Floor'}>Floor</Option>
+                <Option value={'Building'}>Building</Option>
+                <Option value={'Office'}>Office</Option>
+                <Option value={'JobTitle'}>Job Title</Option>
+                <Option value={'WorkPlace'}>Work Place</Option>
               </Select>
             </Form.Item>
-            <Form.Item
-              label="ادخل البيانات"
-              name="text"
-              rules={[{ required: true, message: "Required!" }]}
-            >
-              <Input />
-            </Form.Item>
-            {value === "JobTitle" && (
+            {value !== 'JobTitle' ||
+              value !==
+                'WorkPlace'(
+                  <Form.Item
+                    label='ادخل البيانات'
+                    name='text'
+                    rules={[{ required: true, message: 'Required!' }]}
+                  >
+                    <Input />
+                  </Form.Item>
+                )}
+            {value === 'JobTitle' && (
               <Form.Item
-                label="(اختياري) ItemName"
-                name="ItemName"
-                rules={[{ message: "Required!" }]}
+                label='ادخل البيانات'
+                name='text'
+                rules={[{ required: true, message: 'Required!' }]}
               >
-                <Input />
+                <Select
+                  style={{ marginBottom: '10px', display: 'block' }}
+                  showSearch
+                  allowClear
+                  placeholder='Select a JobTitle'
+                  optionFilterProp='children'
+                >
+                  {isLoading ? (
+                    <>loading...</>
+                  ) : (
+                    uniqueArray.map((data, index) => {
+                      return <Option key={index}>{data}</Option>;
+                    })
+                  )}
+                </Select>
               </Form.Item>
             )}
-            {value === "Name" ||
-              (value === "WorkPlace" && (
-                <Form.Item
-                  label="(اختياري) ItemName"
-                  name="ItemName"
-                  rules={[{ message: "Required!" }]}
+            {value === 'JobTitle' && (
+              <Form.Item
+                label='ادخل البيانات'
+                name='ItemName'
+                rules={[{ required: true, message: 'Required!' }]}
+              >
+                <Select
+                  style={{ marginBottom: '10px', display: 'block' }}
+                  showSearch
+                  allowClear
+                  placeholder='Select a ItemName'
+                  optionFilterProp='children'
                 >
-                  <Input />
+                  {isLoading ? (
+                    <>loading...</>
+                  ) : (
+                    newItemName.map((data, index) => {
+                      return <Option key={index}>{data}</Option>;
+                    })
+                  )}
+                </Select>
+              </Form.Item>
+            )}
+            {value === 'Name' ||
+              (value === 'WorkPlace' && (
+                <Form.Item
+                  label='ادخل البيانات'
+                  name='ItemName'
+                  rules={[{ required: true, message: 'Required!' }]}
+                >
+                  <Select
+                    style={{ marginBottom: '10px', display: 'block' }}
+                    showSearch
+                    allowClear
+                    placeholder='Select a ItemName'
+                    optionFilterProp='children'
+                  >
+                    {isLoading ? (
+                      <>loading...</>
+                    ) : (
+                      newItemName.map((data, index) => {
+                        return <Option key={index}>{data}</Option>;
+                      })
+                    )}
+                  </Select>
                 </Form.Item>
               ))}
-            <div style={{ textAlign: "center" }}>
+            <div style={{ textAlign: 'center' }}>
               <Button
-                style={{ borderRadius: "5px", width: "150px" }}
-                type="primary"
-                htmlType="submit"
-                size={"large"}
+                style={{ borderRadius: '5px', width: '150px' }}
+                type='primary'
+                htmlType='submit'
+                size={'large'}
               >
                 استعلام
               </Button>
