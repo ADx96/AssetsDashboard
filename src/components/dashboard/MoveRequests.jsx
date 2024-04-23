1cd3bc47b7d33090a7aac0171101d6c625e6d0d9
Abduljawad AbuAlrous <abduljawad.abualrous@kw.zain.com>
2024-04-23 23:32:38 +0300

    transfer fix

--- src/components/dashboard/MoveRequests.jsx
+++ src/components/dashboard/MoveRequests.jsx
@@ -1,169 +1,187 @@
-import { Table, Button, Popconfirm, Space, message } from "antd";
-import qs from "qs";
-import React, { useState } from "react";
-import {
-  useGetAssetQuery,
-  useUpdateAssetMutation,
-} from "../../Redux/Api/AssetsApi";
+import { Table, Button, Popconfirm, Space, message } from 'antd'
+import qs from 'qs'
+import React, { useState } from 'react'
+import { useGetAssetQuery, useUpdateAssetMutation } from '../../Redux/Api/AssetsApi'
 import {
   useGetMoveRequestsQuery,
   useUpdateMoveRequestMutation,
-  useDeleteMoveRequestMutation,
-} from "../../Redux/Api/RequestApi";
-
-const query = qs.stringify({
-  populate: "employee",
-});
+  useDeleteMoveRequestMutation
+} from '../../Redux/Api/RequestApi'
 
 const MoveRequests = () => {
-  const { success } = message;
-  const [serial, setSerial] = useState("");
-  const { data, isLoading } = useGetMoveRequestsQuery(query);
-  const [updateMoveRequest] = useUpdateMoveRequestMutation();
-  const [deleteMoveRequest] = useDeleteMoveRequestMutation();
+  const { success } = message
+  const [currentPage, setCurrentPage] = useState()
+  const query = qs.stringify(
+    {
+      populate: 'employee',
+      sort: ['id:dsc'],
+      pagination: {
+        page: currentPage,
+        pageSize: 10
+      }
+    },
+    {
+      encodeValuesOnly: true // prettify URL
+    }
+  )
 
-  const [updateAsset] = useUpdateAssetMutation();
+  const [serial, setSerial] = useState('')
+  const { data, isLoading } = useGetMoveRequestsQuery(query)
+  const [updateMoveRequest] = useUpdateMoveRequestMutation()
+  const [deleteMoveRequest] = useDeleteMoveRequestMutation()
+
+  const [updateAsset] = useUpdateAssetMutation()
+
+  const checkLength =
+    serial.length > 5
+      ? {
+          $contains: serial
+        }
+      : { $eq: serial }
 
   const query2 = qs.stringify(
     {
       filters: {
         Serial: {
-          $contains: serial,
-        },
-      },
+          ...checkLength
+        }
+      }
     },
     {
-      encodeValuesOnly: true, // prettify URL
+      encodeValuesOnly: true // prettify URL
     }
-  );
+  )
 
-  const { data: id } = useGetAssetQuery(query2);
+  const { data: id } = useGetAssetQuery(query2)
 
   const ApiData = data?.data.map((data) => {
-    const ReqId = data.id;
-    const employee = data.attributes.employee.data?.attributes;
-    return { ReqId, ...data.attributes, ...employee };
-  });
+    const ReqId = data.id
+    const employee = data.attributes.employee.data?.attributes
+    return { ReqId, ...data.attributes, ...employee }
+  })
 
+  const total = data?.meta.pagination.total
+  const PageSize = data?.meta.pagination.pageSize
   const handleDelete = async (ReqId) => {
-    deleteMoveRequest(ReqId);
-    success("تم الحذف بنجاح");
-  };
+    deleteMoveRequest(ReqId)
+    success('تم الحذف بنجاح')
+  }
   const columns = [
     {
-      title: "Employee Name(القديم)",
-      dataIndex: "Name",
-      key: "Name",
-      align: "center",
+      title: 'Employee Name(القديم)',
+      dataIndex: 'Name',
+      key: 'Name',
+      align: 'center'
     },
     {
-      title: "Employee I.D",
-      dataIndex: "EmployeeId",
-      key: "EmployeeId",
-      align: "center",
+      title: 'Employee I.D',
+      dataIndex: 'EmployeeId',
+      key: 'EmployeeId',
+      align: 'center'
     },
     {
-      title: "Moved Employee Name(الجديد)",
-      dataIndex: "MoveEmployee",
-      key: "MoveEmployee",
-      align: "center",
+      title: 'Moved Employee Name(الجديد)',
+      dataIndex: 'MoveEmployee',
+      key: 'MoveEmployee',
+      align: 'center'
     },
     {
-      title: "Reason",
-      dataIndex: "Reason",
-      key: "Reason",
-      align: "center",
+      title: 'Reason',
+      dataIndex: 'Reason',
+      key: 'Reason',
+      align: 'center'
     },
     {
-      title: "SERIAL NUMBER",
-      dataIndex: "ItemSerial",
-      key: "ItemSerial",
-      align: "center",
+      title: 'SERIAL NUMBER',
+      dataIndex: 'ItemSerial',
+      key: 'ItemSerial',
+      align: 'center'
     },
     {
-      title: "isApproved",
-      dataIndex: "isApproved",
-      align: "center",
-      key: "isApproved",
+      title: 'isApproved',
+      dataIndex: 'isApproved',
+      align: 'center',
+      key: 'isApproved',
       render: (value) => {
-        return value ? "Approved" : "Pending";
-      },
+        return value ? 'Approved' : 'Pending'
+      }
     },
     {
-      title: "createdAt",
-      dataIndex: "createdAt",
-      key: "createdAt",
-      align: "center",
+      title: 'createdAt',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      align: 'center'
     },
     {
-      title: "Action",
-      align: "center",
-      dataIndex: "operation",
-      key: "operation",
+      title: 'Action',
+      align: 'center',
+      dataIndex: 'operation',
+      key: 'operation',
       render: (_, record) => {
-        const newRecord = (record.ItemSerial = record.ItemSerial.replace(
-          /[\t\n\s]+/g,
-          ""
-        ));
+        const newRecord = (record.ItemSerial = record.ItemSerial.replace(/[\t\n\s]+/g, ''))
 
         const update = {
           id,
           Submit: {
             employee: record.Trans_EmpId,
-            status: "transferred",
-          },
-        };
+            status: 'transferred'
+          }
+        }
 
         return (
-          <div style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
             <Space>
               <Popconfirm
                 disabled={record.isApproved}
                 title="Sure to update?"
                 onConfirm={() => {
-                  updateAsset(update);
-                  updateMoveRequest(record.ReqId);
+                  updateAsset(update)
+                  updateMoveRequest(record.ReqId)
                 }}
               >
                 <Button
                   disabled={record.isApproved}
                   onClick={() => setSerial(newRecord)}
                   style={{
-                    marginRight: 8,
+                    marginRight: 8
                   }}
                 >
                   قبول
                 </Button>
               </Popconfirm>
 
-              <Popconfirm
-                title="Sure to delete?"
-                onConfirm={() => handleDelete(record.ReqId)}
-              >
+              <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.ReqId)}>
                 <Button>Delete</Button>
               </Popconfirm>
             </Space>
           </div>
-        );
-      },
-    },
-  ];
+        )
+      }
+    }
+  ]
 
   if (isLoading) {
-    return <h1>loading</h1>;
+    return <h1>loading</h1>
   }
 
   return (
-    <div style={{ overflow: "auto" }}>
+    <div style={{ overflow: 'auto' }}>
       <Table
-        rowClassName={() => "editable-row"}
+        rowClassName={() => 'editable-row'}
         bordered
+        pagination={{
+          total: total,
+          PageSize: PageSize,
+          onChange: (page) => {
+            setCurrentPage(page)
+          }
+        }}
         rowKey={(record) => record.ReqId}
         dataSource={ApiData}
         columns={columns}
       />
     </div>
-  );
-};
+  )
+}
 
-export default MoveRequests;
+export default MoveRequests
